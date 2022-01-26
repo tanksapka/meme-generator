eeda207042c9646c3ed804d58a63f571192eb2ba
Tamas Schell <tanksapka@yahoo.com>
2022-01-26 22:04:33 +0100

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="./trollface.svg" alt="Problem?" />
+      Problem?
+    </div>
+  );
+}
+
+export default App;
